bcbe3b949a0061cc08f42736373f813046cb699a
Erik Mossberg <lingonvecka@gmail.com>
2017-07-14 11:43:12 +0200

    update to newer version of safemath and ownable from openzeppelin

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -3,18 +3,27 @@ pragma solidity ^0.4.11;
 
 /*
  * Copyright (c) 2016 Smart Contract Solutions, Inc.
- * Ownable
  *
- * Base contract with an owner.
- * Provides onlyOwner modifier, which prevents function from running if it is called by anyone other than the owner.
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
  */
 contract Ownable {
   address public owner;
 
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
   function Ownable() {
     owner = msg.sender;
   }
 
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
   modifier onlyOwner() {
     if (msg.sender != owner) {
       throw;
@@ -22,6 +31,11 @@ contract Ownable {
     _;
   }
 
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
   function transferOwnership(address newOwner) onlyOwner {
     if (newOwner != address(0)) {
       owner = newOwner;
