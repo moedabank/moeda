8325b38b2f05f4e4496da7128a4dbc88adae941f
Erik Mossberg <lingonvecka@gmail.com>
2017-07-25 13:16:52 +0200

    add missing mock contract

---
+++ contracts/test/MockFundraiser.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.11;
+
+contract MockFundraiser {
+  function tokensPerEth() public returns (uint256) {
+    return 1;
+  }
+
+  function issue(address recipient, uint256 amount) {
+  }
+}
