7f3094b159b4a5251abab88739bb09d289b9ec37
Erik Mossberg <lingonvecka@gmail.com>
2017-08-01 00:54:20 +0200

    add comment to migrationAgent interface

--- contracts/MigrationAgent.sol
+++ contracts/MigrationAgent.sol
@@ -2,5 +2,12 @@ pragma solidity ^0.4.13;
 
 
 contract MigrationAgent {
+  /*
+    A contract implementing this interface is assumed to implement the neccessary
+    access controls. E.g;
+    * token being migrated FROM is the only one allowed to call migrateTo
+    * token being migrated TO has a minting function that can only be called by
+      the migration agent
+  */
   function migrateTo(address beneficiary, uint256 amount) public;
 }
