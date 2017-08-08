f9caa1a89387cfd1c05eda179ab74157a24b2ec4
Erik Mossberg <lingonvecka@gmail.com>
2017-08-08 12:41:25 +0200

    appease linter

--- contracts/test/TestMintingToken.sol
+++ contracts/test/TestMintingToken.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.13;
 import '../MoedaToken.sol';
 
+
 contract TestMintingToken is MoedaToken {
   function create(address recipient, uint256 amount) public {
     super.mint(recipient, amount);
